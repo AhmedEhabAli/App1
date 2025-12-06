5f3a300b0524d4cf2ef54a68b9465b86f3ad4253
Ahmed Ehab <138992100+AhmedEhabAli@users.noreply.github.com>
2025-12-07 01:45:02 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,19 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Contact from "./components/Contact/Contact";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import NotFound from "./components/NotFound/NotFound";
 
-const routes = createBrowserRouter([
+const routes = createHashRouter([
   {
-    path: "/",
+    path: "",
     element: <Layout />,
     children: [
       { index: true, element: <Home /> },
-      { path: "/about", element: <About /> },
-      { path: "/portfolio", element: <Portfolio /> },
-      { path: "/contact", element: <Contact /> },
+      { path: "about", element: <About /> },
+      { path: "portfolio", element: <Portfolio /> },
+      { path: "contact", element: <Contact /> },
       { path: "*", element: <NotFound /> },
     ],
   },
